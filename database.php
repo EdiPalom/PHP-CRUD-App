d77bed5ea0631c92f30336cf7092135ed3e5ddab
Edisson Palomares <palomaredif@gmail.com>
2021-10-09 10:16:52 -0500

    added db connection port

--- database.php
+++ database.php
@@ -1,6 +1,7 @@
 <?php
 
-$db_conn = pg_connect("host=db port=5432 dbname=postgres user=postgres password=pass");
+//$db_conn = pg_connect("host=db port=5432 dbname=postgres user=postgres password=pass");
+$db_conn = pg_connect(getenv("DATABASE_URL"));
 
 if(!$db_conn)
 {
