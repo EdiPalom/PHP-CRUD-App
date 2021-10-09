339a4a948f819c8876e365de5f8fe33845e204b3
Edisson Palomares <palomaredif@gmail.com>
2021-10-09 12:21:50 -0500

    db connection update

--- public/database.php
+++ public/database.php
@@ -1,6 +1,5 @@
 <?php
 
-$db_conn = pg_connect("host=ec2-54-208-96-16.compute-1.amazonaws.com port=5432 dbname=dc6sv2o6apil9t user=giisezugjbnkfk password=c975f93dc4011747f8469d287e476d6058f951435f460fdba76c49a54b7c7e96");
 //$db_conn = pg_connect(getenv("DATABASE_URL"));
 
 if(!$db_conn)
